79a8a6099658312ecaa600ee34e3d1f73cfb99fe
kilewa <kilewageorge230@gmail.com>
2020-07-03 05:09:54 +0300

    forms modified

--- script.js
+++ script.js
@@ -41,3 +41,21 @@ caretScrollToTop.addEventListener("click", function () {
   //});
   $(`html, body`).animate({ scrollTop: 0 }, "slow");
 });
+
+
+ // form animate
+
+ var x = document.getElementById("login");
+ var y = document.getElementById("register");
+ var z = document.getElementById("btn");
+ function register(){
+   x.style.left = "-400px";
+   y.style.left = "50px"
+   z.style.left = "110px"
+ }
+ 
+ function login(){
+   x.style.left = "50px";
+   y.style.left = "450px"
+   z.style.left = "0px"
+ }
